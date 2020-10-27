47f8c24de0cf91fefcdb0254e8aaf44e28c065d6
wvcneto <wvcneto@gmail.com>
2020-10-27 17:55:13 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>NextApp Template</title>
+      </Head>
+
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
